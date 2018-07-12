258b5b4b72afa60b2cd55adb69649d09642086fb
오승일 <osi816@naver.com>
2018-07-12 17:39:49 +0900

    보완

---
+++ anolja/src/main/java/kr/co/anolja/stastics/controller/GameMode.java
@@ -0,0 +1,7 @@
+package kr.co.anolja.stastics.controller;
+
+public interface GameMode {
+	String solo ="solo";
+	String duo = "duo";
+	String squad ="squad";
+}
---
+++ anolja/src/main/java/kr/co/anolja/stastics/controller/Season.java
@@ -0,0 +1,8 @@
+package kr.co.anolja.stastics.controller;
+
+public interface Season {
+	String season4 = "division.bro.official.2018-04";
+	String season5 = "division.bro.official.2018-05";
+	String season6 = "division.bro.official.2018-06";
+	String season7 = "division.bro.official.2018-07";
+}
