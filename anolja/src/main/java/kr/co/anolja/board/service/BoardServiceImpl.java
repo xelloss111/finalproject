f10d89e1554eaba942ba84e75580188f25293efa
USER-PC\USER <xelloss111@naver.com>
2018-07-26 12:06:31 +0900

    게시판 답글 이미지 등록 오류 현상 수정 (sql select key 추가, 저장 경로 잘못된 부분 수정)

--- anolja/src/main/java/kr/co/anolja/board/service/BoardServiceImpl.java
+++ anolja/src/main/java/kr/co/anolja/board/service/BoardServiceImpl.java
@@ -68,7 +68,7 @@ public class BoardServiceImpl implements BoardService {
 				int index = file.getOriginalFilename().lastIndexOf(".");
 				ext = file.getOriginalFilename().substring(index);
 				
-				String saveFileName = "board -" + UUID.randomUUID().toString() + ext;
+				String saveFileName = "board-" + UUID.randomUUID().toString() + ext;
 				
 				file.transferTo(new File("c:/pilseong/upload/" + saveFileName));
 				
@@ -144,7 +144,7 @@ public class BoardServiceImpl implements BoardService {
 				
 				String saveFileName = "board-" + UUID.randomUUID().toString() + ext;
 				
-				file.transferTo(new File("c:/java-lec/upload/" + saveFileName));
+				file.transferTo(new File("c:/pilseong/upload/" + saveFileName));
 				
 				boardFile.setPath("c:/pilseong/upload/");
 				boardFile.setFileSize((int) fileSize);
